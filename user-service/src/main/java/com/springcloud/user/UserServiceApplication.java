85fe94275887703780e05eae05929449fd50720b
Zhi Li <zhi.li@invoicefair.com>
2020-09-22 22:13:40 +0100

    [feat] - nacos config

--- user-service/src/main/java/com/springcloud/user/UserServiceApplication.java
+++ user-service/src/main/java/com/springcloud/user/UserServiceApplication.java
@@ -1,14 +1,25 @@
 package com.springcloud.user;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@EnableDiscoveryClient
+@RefreshScope
 @SpringBootApplication
 public class UserServiceApplication {
+
+  @Value("${api.key}")
+  private String apiKey;
+
+  @Value("${common.name}")
+  private String commonName;
 
   public static void main(String[] args) {
     SpringApplication.run(UserServiceApplication.class, args);
@@ -18,6 +29,12 @@ public class UserServiceApplication {
   public ResponseEntity<String> test() {
 
     return ResponseEntity.ok("user-service");
+  }
+
+  @GetMapping("/configs")
+  public ResponseEntity<String> config() {
+
+    return ResponseEntity.ok(apiKey + " " + commonName);
   }
 
 }
